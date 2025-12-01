a6f9f925f291c5146a1870338313777fcd11b092
fsiwinski <dev.siwy@gmail.com>
2025-12-01 11:14:41 +0100

    2nd

---
+++ src/Square.java
@@ -0,0 +1,36 @@
+public class Square extends Rectangle {
+
+    public Square() {}
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return width; // width == length in Square
+    }
+
+    public void setSide(double side) {
+        this.width = side;
+        this.length = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square[" + super.toString() + "]";
+    }
+}
